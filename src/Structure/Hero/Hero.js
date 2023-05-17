1f2868336dc967c31e1a492987c7a1d883d9fb3c
sergii-yastremskyi <theyastremsky@gmail.com>
2023-05-17 21:50:09 +0300

    hero gradient

--- src/Structure/Hero/Hero.js
+++ src/Structure/Hero/Hero.js
@@ -4,6 +4,7 @@ export default function Hero() {
   return (
     <div className={css.hero}>
       <div className={css.heroInsidecontainer}>
+        <div className={css.gradient}></div>
         <h2 className={css.heroHeader}>Оренда фотобудок і гарного настрою!</h2>
         <p className={css.heroContent}>
           фотобудка - відмінне доповнення до будь-якого заходу. Ваші гості
